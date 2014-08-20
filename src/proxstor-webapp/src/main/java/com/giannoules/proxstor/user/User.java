3804131882b760783a517d8ff3fc6a2bfdb4dcd1
Jim Giannoules <jgiannoules@gmail.com>
2014-08-20 18:47:19 -0500

    clean up minor formatting and imports

--- src/proxstor-webapp/src/main/java/com/giannoules/proxstor/user/User.java
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/user/User.java
@@ -1,6 +1,5 @@
 package com.giannoules.proxstor.user;
 
-import com.tinkerpop.blueprints.Vertex;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /*
@@ -9,43 +8,27 @@ import javax.xml.bind.annotation.XmlRootElement;
  *
  * 2014/08/19 - updated to accept com.tinkerpop.blueprints.Vertex in constructor
  */
-
-
 @XmlRootElement
 public class User {
+
     public String userId;
     public String firstName;
     public String lastName;
     public String address;
-    
-    public User() {}
-    
-    
-    /*
-    public User(Vertex v) {
-         firstName = v.getProperty("firstName");
-         lastName = v.getProperty("lastName");
-         address = v.getProperty("address");
-         Object id = v.getId();
-         if (id instanceof Long) {            
-            userId = Long.toString((Long) v.getId());
-         } else {
-             userId = v.getId().toString();
-         }
-        
+
+    public User() {
     }
-    */
-    
+
     public User(String firstName, String lastName, String address) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
     }
-    
+
     public String getUserId() {
         return userId;
     }
-    
+
     public void setUserId(String userId) {
         this.userId = userId;
     }
@@ -73,5 +56,5 @@ public class User {
     public void setAddress(String address) {
         this.address = address;
     }
-    
+
 }
