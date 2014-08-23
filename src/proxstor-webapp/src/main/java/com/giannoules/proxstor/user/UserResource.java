09cca9a130f99c9f415f6ff4427080c9c12b8511
Jim Giannoules <jgiannoules@gmail.com>
2014-08-23 18:26:26 -0500

    @GET, @PUT, @DELETE now return HTTP status

--- src/proxstor-webapp/src/main/java/com/giannoules/proxstor/user/UserResource.java
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/user/UserResource.java
@@ -10,18 +10,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 public class UserResource {
-    
+
     private final String userId;
-    
+
     /*
-     * all these Paths assume userId context, so stash in constructor
+     * all these Paths assume a single userId context, so stash in constructor
      */
     public UserResource(String userId) {
         this.userId = userId;
     }
-    
+
     /*
      * return DeviceResource handler for specified user
      */
@@ -29,51 +30,62 @@ public class UserResource {
     public DeviceResource getDeviceResource() {
         return new DeviceResource(userId);
     }
-    
+
     /*
      * return KnowResource handler for specified user
      */
     @Path("knows")
     public UserKnowsResource getKnowsResource() {
         return new UserKnowsResource(userId);
-    } 
-    
+    }
+
     /*
      * return the specified userId User
-     * if the userId is invalid throw 404
+     * 
+     * success - return 200 (Ok) and JSON representation User
+     * failure - return 404 (Not Found)
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public User getUser() {
+    public Response getUser() {
         User u = UserDao.instance.getUser(userId);
-        if (u == null){             
-            throw new WebApplicationException(404);
+        if (u == null) {
+            return Response.status(404).build();
         }
-        return u;
+        return Response.ok().entity(u).build();
     }
-    
+
     /*
      * update user
-     * userId path must match userId inside JSON (and be valid ID)
-     * if not, return false
+     * Note: userId path must match userId inside JSON (and be valid ID)
+     * 
+     * sucess - return 204 (No Content)
+     * failure - return 404 (Not Found)
      */
-    @PUT    
+    @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public boolean putUser(User u) {
+    public Response putUser(User u) {
         if ((u.getUserId() != null) && u.getUserId().equals(userId)) {
-            return UserDao.instance.updateUser(u);            
+            if (UserDao.instance.updateUser(u)) {
+                return Response.noContent().build();
+            }
         }
-        return false;
+        return Response.status(404).build();
     }
-    
+
     /*
      * remove userId from database
      *
-     * if userId is not valid user return false
+     * success - return 204 (No Content)
+     * failure - return 404 (Not Found)
      */
     @DELETE
-    public boolean deleteUser() {
-        return UserDao.instance.deleteUser(userId);
+    public Response deleteUser() {
+        if (UserDao.instance.deleteUser(userId)) {
+            return Response.noContent().build();
+        } else {
+            return Response.status(404).build();
+        }
     }
-    
+
 }
