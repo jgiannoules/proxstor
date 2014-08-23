4a31fd21c5378c990f534fe6dfc5ccaf809552cb
Jim Giannoules <jgiannoules@gmail.com>
2014-08-23 13:14:36 -0500

    introduce RESTStatus for richer status returns to clients

---
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/RESTStatus.java
@@ -0,0 +1,60 @@
+package com.giannoules.proxstor;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class RESTStatus {
+    public boolean ok;
+    public String message;
+    public String url;
+
+    public RESTStatus() {}
+    
+    public RESTStatus(boolean ok, String message, String url) {
+        this.ok = ok;
+        this.message = message;
+        this.url = url;
+    }
+    
+    /**
+     * @return the ok
+     */
+    public boolean isOk() {
+        return ok;
+    }
+
+    /**
+     * @param ok the ok to set
+     */
+    public void setOk(boolean ok) {
+        this.ok = ok;
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
