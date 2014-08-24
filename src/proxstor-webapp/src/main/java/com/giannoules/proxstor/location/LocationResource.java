c1bdd54fc633176006d1ecc38d9b3d5d46391933
Jim Giannoules <jgiannoules@gmail.com>
2014-08-23 20:28:41 -0500

    all handlers in LocationResource now use http status

--- src/proxstor-webapp/src/main/java/com/giannoules/proxstor/location/LocationResource.java
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/location/LocationResource.java
@@ -1,25 +1,73 @@
 package com.giannoules.proxstor.location;
 
-import com.giannoules.proxstor.device.DeviceResource;
 import com.giannoules.proxstor.sensor.SensorResource;
-import com.giannoules.proxstor.user.User;
-import com.giannoules.proxstor.user.UserDao;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 public class LocationResource {
 
     private final String locId;
-    
+
     public LocationResource(String locId) {
         this.locId = locId;
     }
+    
+    /*
+     * return the specified locId Location
+     * 
+     * success - return 200 (Ok) and JSON representation Location
+     * failure - return 404 (Not Found)
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getLocation() {
+        Location l = LocationDao.instance.getLocationById(locId);
+        if (l == null) {
+            return Response.status(404).build();
+        }
+        return Response.ok().entity(l).build();
+    }
+
+    /*
+     * update location
+     * Note: locId path must match locId inside JSON (and be valid ID)
+     * 
+     * sucess - return 204 (No Content)
+     * failure - return 404 (Not Found)
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response putLocation(Location l) {
+        if ((l.getLocId() != null) && l.getLocId().equals(locId)) {
+            if (LocationDao.instance.updateLocation(l)) {
+                return Response.noContent().build();
+            }
+        }
+        return Response.status(404).build();
+    }
+
+    /*
+     * remove locId from database
+     *
+     * success - return 204 (No Content)
+     * failure - return 404 (Not Found)
+     */
+    @DELETE
+    public Response deleteLocation() {
+        if (LocationDao.instance.deleteLocation(locId)) {
+            return Response.noContent().build();
+        } else {
+            return Response.status(404).build();
+        }
+    }
+
+    // ---- BEGIN sub-resource locators
     
     /*
      * return SensorResource handler for this Location
@@ -27,43 +75,6 @@ public class LocationResource {
     @Path("sensors")
     public SensorResource getSensorResource() {
         return new SensorResource(locId);
-    } 
-    
-   /*
-     * return the specified locId Location
-     * if the locId is invalid throw 404
-     */
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Location getLocation() {
-        Location l = LocationDao.instance.getLocationById(locId);
-        if (l == null){             
-            throw new WebApplicationException(404);
-        }
-        return l;
-    }
-    
-    /*
-     * update location
-     * locIdpath must match locId inside JSON (and be valid ID)
-     * if not, return false
-     */
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)    
-    public boolean putLocation(Location l) {
-        if ((l.getLocId() != null) && l.getLocId().equals(locId)) {
-            return LocationDao.instance.updateLocation(l);            
-        }
-        return false;        
     }
 
-    /*
-     * remove locId from database
-     *
-     * if locId is not valid user return false
-     */
-    @DELETE    
-    public boolean deleteLocation() {
-        return LocationDao.instance.deleteLocation(locId);
-    }
 }
