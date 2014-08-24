d15ec050342f9a893abbd9977edc4c4fe09cb74b
Jim Giannoules <jgiannoules@gmail.com>
2014-08-24 14:45:03 -0500

    KnowsResource simplified

--- src/proxstor-webapp/src/main/java/com/giannoules/proxstor/knows/KnowsResource.java
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/knows/KnowsResource.java
@@ -1,11 +1,17 @@
 package com.giannoules.proxstor.knows;
 
+import com.giannoules.proxstor.ProxStorDebug;
 import com.giannoules.proxstor.user.User;
 import com.giannoules.proxstor.user.UserDao;
 import java.util.List;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /*
  * handles answering requests regarding *who knows userId*
@@ -13,17 +19,51 @@ import javax.ws.rs.core.MediaType;
 public class KnowsResource {
     
     private final String userId;
+    private final Float strength;
     
-    public KnowsResource(String userId) {
+    public KnowsResource(String userId, Float strength) {
         this.userId = userId;
+        this.strength = strength;
     }
     
     /*
-     * return all Users who know userId
+     * return all Users userId knows with at least minimum strength
      */ 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<User> getKnowsUsers() {
-        return UserDao.instance.getKnowsUser(userId);
+    public Response getKnownUsers() {
+        ProxStorDebug.println("getKnownUsers(): " + userId + " min strength " + strength);
+        return Response.ok().build();
+    }
+    
+    /*
+     * returns all Users who know userId with at least minimum strength
+     */
+    @Path("reverse")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getKnowsUsers() {
+        ProxStorDebug.println("getKnowsUsers(): " + userId + " with min strength " + strength);
+        return Response.ok().build();
+    }
+    
+    /*
+     * establish that userId knows otherUser with strength
+     */
+    @Path("{otheruser: [0-9]+}")
+    @POST    
+    public Response establishUserKnows(@PathParam("otheruser") String otherUserId) {
+        ProxStorDebug.println("establishUserKnows(): " + userId + " --Knows [" + strength  + "]--> " + otherUserId);
+        return Response.ok().build();
+    }
+    
+    /*
+     * update the relationship that userId knows otherUser with strength
+     */
+    @Path("{otheruser: [0-9]+}")
+    @PUT    
+    public Response updateUserKnows(@PathParam("otheruser") String otherUserId) {
+        ProxStorDebug.println("updateUserKnows(): " + userId + " --Knows [" + strength  + "]--> " + otherUserId);
+        return Response.ok().build();
     }
 }
