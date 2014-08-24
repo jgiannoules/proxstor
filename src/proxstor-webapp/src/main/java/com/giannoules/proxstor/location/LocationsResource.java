0d4ad0d610ac45564ca1750c7aadba9f9529b96f
Jim Giannoules <jgiannoules@gmail.com>
2014-08-23 20:19:45 -0500

    return proper http response when creating a new location; remove @GET all locations

--- src/proxstor-webapp/src/main/java/com/giannoules/proxstor/location/LocationsResource.java
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/location/LocationsResource.java
@@ -1,7 +1,11 @@
 package com.giannoules.proxstor.location;
 
 import com.giannoules.proxstor.exception.ProxStorGraphDatabaseNotRunningException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,9 +13,40 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/locations")
 public class LocationsResource {
+
+    /*
+     * adds location to database
+     *
+     * returns instance of added Location
+     *
+     * success - returns 201 (Created) with URI of new Location and Location JSON in the body
+     * failure - returns 400 (Bad Request) if the Location could not be added
+     *           returns 500 (Server Error) if the Location could was added and 
+     *                                      URI building error occurred
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response postLocation(Location in) {
+        Location l = LocationDao.instance.addLocation(in);
+        if (l == null) {
+            return Response.status(400).build();
+        } else {
+            try {
+                URI createdUri = new URI("locations/" + l.getLocId());
+                return Response.created(createdUri).entity(l).build();
+            } catch (URISyntaxException ex) {
+                Logger.getLogger(LocationsResource.class.getName()).log(Level.SEVERE, null, ex);
+                return Response.serverError().build();
+            }
+        }
+    }
+
+    // ---- BEGIN sub-resource locators ----
     
     /*
      * returns instance of LocationResource to handle locId specific request 
@@ -20,25 +55,5 @@ public class LocationsResource {
     public LocationResource getLocationResource(@PathParam("locid") String locId) {
         return new LocationResource(locId);
     }
-    
-    /*
-     * returns all locations system-wide!
-     */
-    @GET
-    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
-    public Collection<Location> getLocations() {
-        return LocationDao.instance.getAllLocations();
-    }
 
-    /*
-     * adds location to database
-     *
-     * returns instance of added Location
-     */
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Location postLocation(Location l) {
-        return LocationDao.instance.addLocation(l);
-    }
 }
