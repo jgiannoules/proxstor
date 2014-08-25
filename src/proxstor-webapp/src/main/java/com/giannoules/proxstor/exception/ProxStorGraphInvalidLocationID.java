6350cf50e8c8f1444322352fa169ffd83fe9fe5b
Jim Giannoules <jgiannoules@gmail.com>
2014-08-24 19:03:26 -0500

    push back with invalid LocationID

---
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/exception/ProxStorGraphInvalidLocationID.java
@@ -0,0 +1,9 @@
+package com.giannoules.proxstor.exception;
+
+public class ProxStorGraphInvalidLocationID extends Exception {
+    
+    public ProxStorGraphInvalidLocationID(String s) {
+        super(s);
+    }
+    
+}
