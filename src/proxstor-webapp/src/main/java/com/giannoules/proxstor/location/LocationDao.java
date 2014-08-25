af3f23270957e7cf10e7aeb6cee70703a0ed951d
Jim Giannoules <jgiannoules@gmail.com>
2014-08-24 19:03:52 -0500

    create/remove Within relationships

--- src/proxstor-webapp/src/main/java/com/giannoules/proxstor/location/LocationDao.java
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/location/LocationDao.java
@@ -2,7 +2,11 @@ package com.giannoules.proxstor.location;
 
 import com.giannoules.proxstor.ProxStorGraph;
 import com.giannoules.proxstor.exception.ProxStorGraphDatabaseNotRunningException;
+import com.giannoules.proxstor.exception.ProxStorGraphInvalidLocationID;
 import com.giannoules.proxstor.exception.ProxStorGraphNonExistentObjectID;
+import static com.tinkerpop.blueprints.Direction.IN;
+import static com.tinkerpop.blueprints.Direction.OUT;
+import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.GraphQuery;
 import com.tinkerpop.blueprints.Vertex;
 import java.util.ArrayList;
@@ -121,7 +125,7 @@ public enum LocationDao {
         }
         return devices;
     }
-    
+
     /*
      * find all matching Locations based on partially specified Location
      */
@@ -232,6 +236,52 @@ public enum LocationDao {
                 return false;
             }
             return true;
+        }
+        return false;
+    }
+
+    /*
+     *
+     * @TODO don't recreate within if already exists
+     */
+    public boolean establishLocationWithinLocation(String innerLocId, String outerLocId) throws ProxStorGraphInvalidLocationID {
+        if (!validLocationId(innerLocId) || !validLocationId(outerLocId)) {
+            throw new ProxStorGraphInvalidLocationID("at least one location ID is invalid");
+        }
+        try {
+            Vertex outVertex = ProxStorGraph.instance.getVertex(innerLocId);
+            Vertex inVertex = ProxStorGraph.instance.getVertex(outerLocId);
+            ProxStorGraph.instance.addEdge(outVertex, inVertex, "within");
+            return true;
+        } catch (ProxStorGraphDatabaseNotRunningException | ProxStorGraphNonExistentObjectID ex) {
+            Logger.getLogger(LocationDao.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    /*
+     * removes an established Within relationship from in -> out
+     *
+     * returns true if succesful
+     * returns false if either locId is invalid or if a Within relationship
+     * was not already established 
+     */
+    public boolean removeLocationWithinLocation(String innerLocId, String outerLocId) throws ProxStorGraphInvalidLocationID {
+        if (!validLocationId(innerLocId) || !validLocationId(outerLocId)) {
+            throw new ProxStorGraphInvalidLocationID("at least one location ID is invalid");
+        }
+        Vertex v;
+        try {
+            v = ProxStorGraph.instance.getVertex(innerLocId);
+        } catch (ProxStorGraphDatabaseNotRunningException | ProxStorGraphNonExistentObjectID ex) {
+            Logger.getLogger(LocationDao.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        for (Edge e : v.getEdges(OUT, "within")) {
+            if (e.getVertex(IN).getId().toString().equals(outerLocId)) {
+                e.remove();
+                return true;
+            }
         }
         return false;
     }
