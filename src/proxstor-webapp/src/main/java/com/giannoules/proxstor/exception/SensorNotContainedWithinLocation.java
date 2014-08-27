d13fa41bb77c184e46e16556a1376aefa1c87c65
Jim Giannoules <jgiannoules@gmail.com>
2014-08-27 14:48:29 -0500

    can now throw exception indicating that a sensor is not within a location

---
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/exception/SensorNotContainedWithinLocation.java
@@ -0,0 +1,12 @@
+package com.giannoules.proxstor.exception;
+
+public class SensorNotContainedWithinLocation extends Exception {
+    
+    public SensorNotContainedWithinLocation(String s) {
+        super(s);
+    }
+    
+    public SensorNotContainedWithinLocation() {
+        super();
+    }
+}
