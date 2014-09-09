9c24f42d39f0365dc0b46c56882d91b626cc9deb
Jim Giannoules <jgiannoules@gmail.com>
2014-09-09 13:39:24 -0500

    add boolean to control debug printing. setting to off by default.

--- src/proxstor-webapp/src/main/java/com/giannoules/proxstor/ProxStorDebug.java
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/ProxStorDebug.java
@@ -1,10 +1,15 @@
 package com.giannoules.proxstor;
 
 // @TODO document
-
+ 
 public class ProxStorDebug {
     
+    private static boolean DEBUG_ON = false;
+    
     public static void println(String... msgs) {
+        if (!DEBUG_ON) {
+            return;
+        }
         System.out.print("<< ProxStor Debug Print >> ");
         for (int x = 0; x < msgs.length - 1; x++)         
             System.out.print(msgs[x] + " ");
