0b00c196eb1daae5798c00f3e74199c1122c759e
Jim Giannoules <jgiannoules@gmail.com>
2014-09-17 20:38:39 -0500

    added new exception - InvalidLocalityId

---
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/exception/InvalidLocalityId.java
@@ -0,0 +1,13 @@
+package com.giannoules.proxstor.exception;
+
+public class InvalidLocalityId extends InvalidParameter {
+    
+    public InvalidLocalityId(String s) {
+        super(s);
+    }
+    
+    public InvalidLocalityId() {
+        super();
+    }
+    
+}
