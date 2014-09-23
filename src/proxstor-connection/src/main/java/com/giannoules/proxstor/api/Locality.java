5c31bf6ef119e298240f8afbd4929abefcfabf3c
Jim Giannoules <jgiannoules@gmail.com>
2014-09-22 22:17:08 -0500

    DateTime -> Date due to serialization issues (the proxstor backend will continue to use Joda time DateTime)

--- src/proxstor-connection/src/main/java/com/giannoules/proxstor/api/Locality.java
+++ src/proxstor-connection/src/main/java/com/giannoules/proxstor/api/Locality.java
@@ -1,7 +1,8 @@
 package com.giannoules.proxstor.api;
 
+import java.util.Date;
+import java.util.Objects;
 import javax.xml.bind.annotation.XmlRootElement;
-import org.joda.time.DateTime;
 
 @XmlRootElement
 public class Locality {
@@ -15,8 +16,10 @@ public class Locality {
     public boolean manual;         // manual entry?
     public boolean active;         // is locality currently active?
 
-    public DateTime arrival;       // arrival time
-    public DateTime departure;     // departure time
+//    public DateTime arrival;       // arrival time
+//    public DateTime departure;     // departure time
+    public Date arrival;       // arrival time
+    public Date departure;     // departure time
 
     
     public Locality() {
@@ -70,19 +73,19 @@ public class Locality {
         this.locationId = locationId;
     }
 
-    public DateTime getArrival() {
+    public Date getArrival() {
         return arrival;
     }
 
-    public void setArrival(DateTime arrive) {
+    public void setArrival(Date arrive) {
         this.arrival = arrive;
     }
 
-    public DateTime getDeparture() {
+    public Date getDeparture() {
         return departure;
     }
 
-    public void setDeparture(DateTime departure) {
+    public void setDeparture(Date departure) {
         this.departure = departure;
     }
     
@@ -96,6 +99,63 @@ public class Locality {
         sb.append("arrive: ").append(arrival).append(" ");
         sb.append("depart: ").append(departure);
         return sb.toString();
+    }
+    
+    @Override
+    public int hashCode() {
+        int hashCode = locationId.hashCode();
+        if (arrival != null) {
+            hashCode *= arrival.hashCode();
+        }
+        if (departure != null) {
+            hashCode *= departure.hashCode();
+        }
+        if (deviceId != null) {
+            hashCode *= deviceId.hashCode();
+        }
+        if (sensorId != null) {
+            hashCode *= sensorId.hashCode();
+        }
+        if (active) {
+            hashCode *= 5;
+        }
+        if (manual) {
+            hashCode *= 7;
+        }
+        return hashCode;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+         if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Locality other = (Locality) obj;
+        if (!Objects.equals(this.arrival, other.arrival)) {
+            return false;
+        }
+        if (!Objects.equals(this.departure, other.departure)) {
+            return false;
+        }
+        if (!Objects.equals(this.deviceId, other.deviceId)) {
+            return false;
+        }
+        if (!Objects.equals(this.sensorId, other.sensorId)) {
+            return false;
+        }
+        if (!Objects.equals(this.locationId, other.locationId)) {
+            return false;
+        }
+        if (!Objects.equals(this.active, other.active)) {
+            return false;
+        }
+        if (!Objects.equals(this.manual, other.manual)) {
+            return false;
+        }
+        return true;
     }
     
 }
