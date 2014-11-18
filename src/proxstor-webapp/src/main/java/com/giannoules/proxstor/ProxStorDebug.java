8a9c10bcdc435b74ff2bb42d1d1e23171070936a
Jim Giannoules <jgiannoules@gmail.com>
2014-11-18 08:35:44 -0600

    performance monitoring helper static methods

--- src/proxstor-webapp/src/main/java/com/giannoules/proxstor/ProxStorDebug.java
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/ProxStorDebug.java
@@ -1,5 +1,10 @@
 package com.giannoules.proxstor;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Useful debugging methods provided statically.
  * 
@@ -12,6 +17,10 @@ public class ProxStorDebug {
      * Simple mechanism to control debugging behavior in a global manner
      */
     private static final boolean DEBUG_ON = false;
+    
+    // stats on performance
+    private static Map<String, AtomicLong> perfTotalTime = new ConcurrentHashMap<>();
+    private static Map<String, AtomicInteger> perfTotalCount = new ConcurrentHashMap<>();
     
     /**
      * Print debug string prepended with debug text
@@ -27,5 +36,36 @@ public class ProxStorDebug {
             System.out.print(msgs[x] + " ");
         if (msgs.length > 0)
             System.out.println(msgs[msgs.length - 1]);
+    }
+    
+    public static long startTimer() {
+        return System.currentTimeMillis();
+    }
+    
+    public static long endTimer(String desc, long start) {
+        long duration = System.currentTimeMillis() - start;
+        if (perfTotalCount.get(desc) == null) {
+            perfTotalCount.put(desc, new AtomicInteger());
+        }
+        perfTotalCount.get(desc).incrementAndGet();
+        if (perfTotalTime.get(desc) == null) {
+            perfTotalTime.put(desc, new AtomicLong());
+        }
+        perfTotalTime.get(desc).addAndGet(duration);
+        return duration;
+    }
+    
+    public static String getPerf() {
+        StringBuilder sb = new StringBuilder();
+        for (String k : perfTotalCount.keySet()) {
+            sb.append(k);
+            sb.append(": ");            
+            sb.append(perfTotalCount.get(k).get());
+            sb.append(", ");
+            double avg = perfTotalTime.get(k).doubleValue() / perfTotalCount.get(k).doubleValue();
+            sb.append(avg);
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
