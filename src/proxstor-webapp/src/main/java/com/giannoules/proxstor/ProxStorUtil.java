d7ed2bd0b5dee95cc74914cba9c480b4c69c987c
Jim Giannoules <jgiannoules@gmail.com>
2014-11-26 17:25:37 -0600

    javadoc improvements

--- src/proxstor-webapp/src/main/java/com/giannoules/proxstor/ProxStorUtil.java
+++ src/proxstor-webapp/src/main/java/com/giannoules/proxstor/ProxStorUtil.java
@@ -2,6 +2,12 @@ package com.giannoules.proxstor;
 
 public class ProxStorUtil {
     
+      /**
+       * replace questionable chars in path with percent representation
+       * 
+       * @param path string representation of path to clean up
+       * @return sanitized path
+       */
       public static String cleanPath(String path) {
         String s;
         s = path.replaceAll("#", "%23");
